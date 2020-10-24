760ad089a0a8796c538feb9aaced0f72abe1d1b3
jccastiblancor <jc.castiblancor@uniandes.edu.co>
2020-10-24 16:38:00 -0500

    server

--- server/src/Server.java
+++ server/src/Server.java
@@ -48,16 +48,16 @@ public class Server {
             socket.receive(request);
             System.out.println("Conectado");
 
-            /*String quote = getRandomQuote();
-            byte[] buffer = file.getBytes();
-
-            DatagramPacket response = new DatagramPacket(buffer, buffer.length, clientAddress, clientPort);
-            socket.send(response);*/
-
             InetAddress clientAddress = request.getAddress();
             int clientPort = request.getPort();
 
             File file = new File("./Quotes.txt");
+
+            MessageDigest shaDigest = MessageDigest.getInstance("SHA-256");
+            String shaChecksum = getFileChecksum(shaDigest, file);
+            String send =  file.getName()+","+shaChecksum;
+            DatagramPacket sendPacket = new DatagramPacket(send, send.length, clientAddress, clientPort);
+            socket.send(sendPacket);
 
             int numberPackets = (int) Math.ceil((double) file.length()/(double) 512);
 
@@ -91,6 +91,26 @@ public class Server {
         int randomIndex = random.nextInt(listQuotes.size());
         String randomQuote = listQuotes.get(randomIndex);
         return randomQuote;
+    }
+
+    private static String getFileChecksum(MessageDigest digest, File file) throws IOException {
+        FileInputStream fis = new FileInputStream(file);
+        byte[] byteArray = new byte[1024];
+        int bytesCount = 0;
+
+        while ((bytesCount = fis.read(byteArray)) != -1) {
+            digest.update(byteArray, 0, bytesCount);
+        }
+
+        fis.close();
+        byte[] bytes = digest.digest();
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < bytes.length; i++) {
+            sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+        }
+
+        return sb.toString();
     }
 
 
