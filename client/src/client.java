f6786a2b9b2592bacd3dfbe0f70cfb497d52a472
maescalante <ma.escalante@uniandes.edu.co>
2020-10-25 20:59:46 -0500

    kdkfjk

--- client/src/client.java
+++ client/src/client.java
@@ -20,19 +20,26 @@ public class client {
             System.out.println("Connected")
             while (true) {
 
-               
+
 
 
                 DatagramPacket request = new DatagramPacket(new byte[1], 1, address, port);
                 socket.send(request);
-
                 byte[] buffer = new byte[512];
+                DatagramPacket response= new DatagramPacket(buffer, buffer.length);
+                String name_checksum= new String (buffer,0,response.getLength());
+                String[] splitted= name_checksum.split(",");
+                name= splittled[0];
+                checksum=splitted[1];
+
+                buffer = new byte[512];
                 DatagramPacket response = new DatagramPacket(buffer, buffer.length);
                 socket.receive(response);
-
+                FileOutputStream outFile = new FileOutputStream("./" + name);
                 String quote = new String(buffer, 0, response.getLength());
 
-                System.out.println(quote);
+                //System.out.println(quote);
+
                 System.out.println();
 
                 Thread.sleep(10000);
@@ -47,5 +54,46 @@ public class client {
         } catch (InterruptedException ex) {
             ex.printStackTrace();
         }
+    }
+    public static void writeLog(String msj) throws IOException {
+        FileWriter fw = new FileWriter("./clientLog_" + bufferSize + ".txt", true);
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+
+        fw.write(dtf.format(now) + msj + "\n");
+
+        fw.close();
+    }
+
+    private static String getFileChecksum(MessageDigest digest, File file) throws IOException
+    {
+
+        FileInputStream fis = new FileInputStream(file);
+
+
+        byte[] byteArray = new byte[1024];
+        int bytesCount = 0;
+
+
+        while ((bytesCount = fis.read(byteArray)) != -1) {
+            digest.update(byteArray, 0, bytesCount);
+        };
+
+
+        fis.close();
+
+
+        byte[] bytes = digest.digest();
+
+
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i< bytes.length ;i++)
+        {
+            sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+        }
+
+        //return complete hash
+        return sb.toString();
     }
 }
