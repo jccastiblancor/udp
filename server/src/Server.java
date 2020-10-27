ea86e91cd2f2dffe23ad6462ae65ba9d1b1434fe
maescalante <ma.escalante@uniandes.edu.co>
2020-10-27 10:40:33 -0500

    ahg

--- server/src/Server.java
+++ server/src/Server.java
@@ -1,60 +1,125 @@
-#!/usr/bin/env python
+import java.io.*;
+import java.net.*;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
-from __future__ import division
-import cv2
-import numpy as np
-import socket
-import struct
-import math
+/**
+ * This program demonstrates how to implement a UDP server program.
+ *
+ *
+ * @author www.codejava.net
+ */
+public class Server {
+    private DatagramSocket socket;
+    private List<String> listQuotes = new ArrayList<String>();
+    private Random random;
+
+    public Server(int port) throws SocketException, IOException {
+        socket = new DatagramSocket(port);
+        random = new Random();
+    }
+
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            System.out.println("Syntax: Server <file> <port>");
+            return;
+        }
+
+        String quoteFile = args[0];
+        int port = Integer.parseInt(args[1]);
+
+        try {
+            Server server = new Server(port);
+            server.loadQuotesFromFile(quoteFile);
+            //writeLog(" Server is listening on port " + port);
+            System.out.println("Server is listening on port " + port);
+            server.service();
+        } catch (SocketException ex) {
+            System.out.println("Socket error: " + ex.getMessage());
+        } catch (IOException ex) {
+            System.out.println("I/O error: " + ex.getMessage());
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void service() throws IOException, NoSuchAlgorithmException {
+        while (true) {
+            DatagramPacket request = new DatagramPacket(new byte[1], 1);
+            socket.receive(request);
+            System.out.println("Conectado");
+
+            InetAddress clientAddress = request.getAddress();
+            int clientPort = request.getPort();
+
+            File file = new File("./Quotes.txt");
+            MessageDigest shaDigest = MessageDigest.getInstance("SHA-256");
+            String shaChecksum = getFileChecksum(shaDigest, file);
+            String send =  file.getName()+","+shaChecksum;
+            DatagramPacket sendPacket = new DatagramPacket(send.getBytes(), send.length(), clientAddress, clientPort);
+            socket.send(sendPacket);
+            int numberPackets = (int) Math.ceil((double) file.length()/(double) 512);
+
+            FileInputStream fis = new FileInputStream(file);
+            BufferedInputStream bis = new BufferedInputStream(fis);
+            byte[] sendData = new byte[512];
+            long current = 0;
+            // Se inicia transmision del archivo hasta que se envien todos los paquetes
+            while(current != numberPackets) {
+                sendData = new byte[512];
+                bis.read(sendData);
+                sendPacket = new DatagramPacket(sendData, sendData.length, clientAddress, clientPort);
+                socket.send(sendPacket);
+                current++;
+            }
+
+        }
 
 
-class FrameSegment(object):
-    """
-    Object to break down image frame segment
-    if the size of image exceed maximum datagram size
-    """
-    MAX_DGRAM = 2**16
-    MAX_IMAGE_DGRAM = MAX_DGRAM - 64 # extract 64 bytes in case UDP frame overflown
-    def __init__(self, sock, port, addr="127.0.0.1"):
-        self.s = sock
-        self.port = port
-        self.addr = addr
-
-    def udp_frame(self, img):
-        """
-        Compress image and Break down
-        into data segments
-        """
-        compress_img = cv2.imencode('.jpg', img)[1]
-        dat = compress_img.tostring()
-        size = len(dat)
-        count = math.ceil(size/(self.MAX_IMAGE_DGRAM))
-        array_pos_start = 0
-        while count:
-            array_pos_end = min(size, array_pos_start + self.MAX_IMAGE_DGRAM)
-            self.s.sendto(struct.pack("B", count) +
-                dat[array_pos_start:array_pos_end],
-                (self.addr, self.port)
-                )
-            array_pos_start = array_pos_end
-            count -= 1
 
 
-def main():
-    """ Top level main function """
-    # Set up UDP socket
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    port = 12345
+    }
 
-    fs = FrameSegment(s, port)
 
-    cap = cv2.VideoCapture(2)
-    while (cap.isOpened()):
-        _, frame = cap.read()
-        fs.udp_frame(frame)
-    cap.release()
-    cv2.destroyAllWindows()
-    s.close()
+    private void loadQuotesFromFile(String quoteFile) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(quoteFile));
+        String aQuote;
 
-if __name__ == "__main__":
-    main()
+        while ((aQuote = reader.readLine()) != null) {
+            listQuotes.add(aQuote);
+        }
+
+        reader.close();
+    }
+
+    private String getRandomQuote() {
+        int randomIndex = random.nextInt(listQuotes.size());
+        String randomQuote = listQuotes.get(randomIndex);
+        return randomQuote;
+    }
+
+    private static String getFileChecksum(MessageDigest digest, File file) throws IOException {
+        FileInputStream fis = new FileInputStream(file);
+        byte[] byteArray = new byte[1024];
+        int bytesCount = 0;
+
+        while ((bytesCount = fis.read(byteArray)) != -1) {
+            digest.update(byteArray, 0, bytesCount);
+        }
+
+        fis.close();
+        byte[] bytes = digest.digest();
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < bytes.length; i++) {
+            sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+        }
+
+        return sb.toString();
+    }
+
+
+}
